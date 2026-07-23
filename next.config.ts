65f2ccb214ecd6a2367ba2a92d5e05eabbb3fb3c
Joseph Feldmann <josfeldmann@gmail.com>
2026-07-22 20:42:03 -0400

    began adding pages crap

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,15 @@ const nextConfig: NextConfig = {
   output: 'export',
   experimental: {
     serverSourceMaps : true
-  }
+  },
+
+   // Required when using next/image without a Next.js server.
+  images: {
+    unoptimized: true,
+  },
+
+  // Optional, but often works more predictably with GitHub Pages.
+  trailingSlash: true,
 };
 
 export default nextConfig;
