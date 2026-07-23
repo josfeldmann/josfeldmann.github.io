f7f6af32c2b26f6ef46291737658d77719cca735
Joseph Feldmann <josfeldmann@gmail.com>
2026-07-23 18:57:36 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,13 +7,7 @@ const nextConfig: NextConfig = {
     serverSourceMaps : true
   },
 
-   // Required when using next/image without a Next.js server.
-  images: {
-    unoptimized: true,
-  },
-
-  // Optional, but often works more predictably with GitHub Pages.
-  trailingSlash: true,
+  
 };
 
 export default nextConfig;
